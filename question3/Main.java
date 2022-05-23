8c5236fa0d388d9f804dd19cfdc9ae62226b66ed
Utsav Gupta <gutsav9@gmail.com>
2022-05-23 15:56:34 +0530

    third commit

--- question3/Main.java
+++ question3/Main.java
@@ -8,10 +8,12 @@ public class Main {
 		System.out.println("Car Model : "+c1.getModel());
 		System.out.println("Car companyName : "+c1.getCompanyName());
 		System.out.println("Car color : "+c1.getColor());
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		System.out.println("Car RPM : "+c1.getEngine().getRmp());
+		System.out.println("Car power : "+c1.getEngine().getPower());
+		System.out.println("Car Engine Manufacture : "+c1.getEngine().getManufacturer());
+		System.out.println("Car Has Turbo : "+c1.getEngine().getHasTurbo());
+		
+		c1.getEngine().enableTurbo();
 
 	}
 
